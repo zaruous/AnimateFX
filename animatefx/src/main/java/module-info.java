b68eb532fb113642c28ca9d06a198d26facba6a0
zaruous <zaruous@naver.com>
2024-01-18 23:35:51 +0900

    update environment java21 and module-info.java

---
+++ animatefx/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module org.animatefx {
+	
+	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.graphics;
+	
+	exports animatefx.animation;
+	exports animatefx.animation.partial;
+	exports animatefx.util;
+}
